fe013895a76eb9cf9288d425dd55c82a0973af52
@DanGustavo1234 <dacordova1234@gmail.com>
2022-10-13 10:38:50 -0500

    version 2

--- src/components/contactos.vue
+++ src/components/contactos.vue
@@ -1,84 +1,19 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <title>Document1</title>
-    </head>
-    <body>
-      <div class="hello">
 
-        <vs-row vs-w="12">
-          
-        <vs-card class="cardx">
-        <vs-row vs-justify="center">
 
-         
-          
-            <vs-col vs-type="flex" vs-justify="center"  vs-align="center"  vs-lg="2"
+
+<vs-row vs-justify="center">
+  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
+<vs-row vs-justify="center">
+  
+    <vs-card class="cardx">
+      <vs-row vs-justify="center">
+      <vs-col vs-type="flex" vs-justify="center"  vs-align="center"  vs-lg="5"
                 vs-sm="4"
-                vs-xs="6">
-                <di>
-                  <h2>Contactos</h2>
-                  <br />
-                  <br />
-                  <vs-input
-                    :success="true"
-                    success-text="The mail is valid"
-                    placeholder="Email Success"
-                  />
-                  <vs-input
-                    :danger="true"
-                    danger-text="The password does not meet the standards"
-                    placeholder="Password Danger"
-                  />
-                  <vs-input
-                    :warning="true"
-                    warning-text="The entered data could not be verified"
-                    placeholder="Date Warning"
-                  />
-                  <vs-input
-                    description-text="Just enter the first two numbers of your phone"
-                    placeholder="Phone Description"
-                  />
-                  <vs-button color="primary" type="filled">Enviar</vs-button>
-                </di>
-                
-    </vs-col>
-    <vs-col vs-type="flex" vs-justify="center"  vs-align="center"  vs-lg="2"
-                vs-sm="4"
-                vs-xs="6">
-                <img
-                  src="../assets/contactos-phones.png"
-                  style="max-width: 100%"
-                />
-    </vs-col>
+                vs-xs="4">
 
-          
-      
-            
-  </vs-row>
-
-</vs-card>
-
-</vs-row>     
-   
-
-       <!--///////////////////////////////////////////////////////////-->
-          <!-- <vs-row vs-w="12">
-            <vs-card class="cardx">
-              <vs-col
-                vs-type="flex"
-                vs-justify="center"
-                vs-align="center"
-                vs-lg="2"
-                vs-sm="4"
-                vs-xs="6"
-              >
-                <div>
+                  <div>
                   <h2>Contactos</h2>
                   <br />
                   <br />
@@ -103,30 +38,34 @@
                   />
                   <vs-button color="primary" type="filled">Enviar</vs-button>
                 </div>
-              </vs-col>
-             
-              <vs-col
-                vs-type="flex"
-                vs-justify="center"
-                vs-align="center"
-                vs-lg="2"
+              
+      </vs-col>
+        <vs-col vs-type="flex" vs-justify="center"  vs-align="center"  vs-lg="7"
                 vs-sm="4"
-                vs-xs="6"
-              >
-                <img
+                vs-xs="4">
+                
+                  <img
                   src="../assets/contactos-phones.png"
-                  style="max-width: 100%"
+                  style="max-width: 90%;max-height: 90%; "
                 />
-              </vs-col>
-            </vs-card>
-          </vs-row> -->
-<!--///////////////////////////////////////////////////////////-->
+                
+                
+        </vs-col>
+      </vs-row>
+    </vs-card>
 
+ 
+  
+</vs-row>
 
+</vs-col>
+</vs-row>
 
-      </div>
-    </body>
-  </html>
+  <!-- ///////////////////////////////////////////////////////// -->
+  
+
+<!-- //////////////////////////////////////////// -->
+  
 </template>
 
 <script lang="ts">
@@ -146,11 +85,9 @@ export default class contactos extends Vue {
 .cardx {
   margin: 15px;
   background-color: #f5f5f5;
+  
  
 }
 
-pre {
-  width: 100%;
-  color: rgba(255, 255, 255, 0.8);
-}
+
 </style>
